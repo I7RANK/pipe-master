b7c7b9124b48e1de7d998b0a3dd483e15a6a13d2
I7RANK <franfgh2000@hotmail.com>
2024-08-08 20:49:06 -0500

    feat: add random rotation to pieces when appear it

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -3,18 +3,32 @@
 import GameHeader from '@/components/GameHeader';
 import Sprite from '@/components/Sprite';
 import { ReactElement } from 'react';
+import { SpritesName } from '@/types/sprites';
 
 export default function Page() {
   const height = 5;
   const width = 5;
   const table: ReactElement<any, any>[] = [];
 
+  const pieces: (SpritesName | '')[][] = [
+    ['curve', 'curve', '', '', ''],
+    ['rect', 'curve', 'curve', '', ''],
+    ['rect', '', 'rect', '', ''],
+    ['start', '', 'end', '', ''],
+    ['', '', '', '', ''],
+  ];
+
   for (let indexY = 0; indexY < height; indexY++) {
     const row: ReactElement<any, any>[] = [];
     for (let indexX = 0; indexX < width; indexX++) {
       row.push(
-        <div key={indexY + indexX} className="bg-red-600 w-full relative">
-          <Sprite spriteName={'doubleCurve'} isPainted />
+        <div key={indexY + indexX} className="bg-[#333649] w-full relative">
+          {pieces[indexY][indexX] !== '' && (
+            <Sprite
+              spriteName={pieces[indexY][indexX] as SpritesName}
+              isPainted
+            />
+          )}
         </div>
       );
     }
@@ -29,9 +43,9 @@ export default function Page() {
     <div className="pt-5 h-screen">
       <GameHeader />
 
-      <div className="relative mx-auto bg-lime-500 max-w-[calc(100vmin_-_16px)] lg:aspect-square max-h-[calc(100vmin_-_16px)] lg:h-4/5">
+      <div className="relative bg-[#5a6288] mx-auto max-w-[calc(100vmin_-_16px)] lg:aspect-square max-h-[calc(100vmin_-_16px)] lg:h-4/5">
         <div className="relative w-full h-full pb-[100%] lg:pb-0 lg:w-auto ">
-          <div className="absolute top-0 left-0 w-full h-full p-1 flex flex-col bg-slate-500/5 gap-y-1">
+          <div className="absolute top-0 left-0 w-full h-full p-1 flex flex-col gap-y-1">
             {table}
           </div>
         </div>
--- components/Sprite.tsx
+++ components/Sprite.tsx
@@ -1,6 +1,8 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import { PIPE_SPRITES } from '@/constants/pipe-pieces';
 import { SpritesName } from '@/types/sprites';
+import { createCircularIterator } from '@/utils/circular-iterator';
+import '@/styles/sprite.css';
 
 type SpriteProps = {
   readonly spriteName: SpritesName;
@@ -12,18 +14,33 @@ export default function Sprite({ spriteName, isPainted }: SpriteProps) {
   const { painted, unPainted } = PIPE_SPRITES;
   const sprite = isPainted ? painted[spriteName] : unPainted[spriteName];
   const [currentRotation, setCurrentRotation] = useState(0);
+  const getPositionIndex = useRef<Function>(() => {});
+  useEffect(() => {
+    getPositionIndex.current = createCircularIterator(sprite.stepsQuantity);
+    getPositionIndex.current();
+    setRandomPosition();
+  }, []);
   const rotate90DegPiece = () => {
+    const position = getPositionIndex.current();
     const rotateTo = currentRotation + 90;
     if (!buttonRef.current) return;
     buttonRef.current.style.transform = `rotate(${rotateTo}deg)`;
     setCurrentRotation(rotateTo);
-    console.log(buttonRef.current.style.transform);
+    console.log(position, buttonRef.current.style.transform);
+  };
+  const setRandomPosition = () => {
+    if (!buttonRef.current) return;
+    const randomNumber = Math.floor(Math.random() * 4);
+    const rotation = randomNumber * 90;
+    buttonRef.current.style.transform = `rotate(${rotation}deg)`;
+    buttonRef.current.classList.add('fade-in');
+    setCurrentRotation(rotation);
   };
 
   return (
     <button
       ref={buttonRef}
-      className="w-[calc(100%+4px)] h-[calc(100%+4px)] -mt-0.5 bg-contain bg-center transition-transform"
+      className="w-[calc(100%+4px)] opacity-0 h-[calc(100%+4px)] -mt-0.5 bg-contain bg-center transition-transform"
       style={{ backgroundImage: `url(${sprite.imgUrl})` }}
       onClick={rotate90DegPiece}
     />
